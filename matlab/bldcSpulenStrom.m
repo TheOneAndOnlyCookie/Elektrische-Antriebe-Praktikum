a740f40073fb2e8862343d1a9ea0227f90b4fa98
Johannes Nill <johannes@nill.me>
2018-05-19 16:06:30 +0200

    Release 1

--- matlab/bldcSpulenStrom.m
+++ matlab/bldcSpulenStrom.m
@@ -1,22 +1,25 @@
 %Spulenstrom
 
 t = [-1:10e-3:10];
-i = 15*(1-exp(-(t/1.375)));
-u = (6 * sign(t)) + 6;
+i = 15*(1-exp(-((t-1)/1.375)));
+u = (6 * sign(t - 1)) + 6;
 
-yyaxis left
+
 plot(t,u)
 ylabel('UU/V')
-yticks([0:3:12])
-axis([-1 10 0 15])
+yticks([0:3:15])
+axis([0 10 0 15])
+grid('on')
 
-yyaxis right
+matlab2tikz
+
+figure;
 plot(t,i)
 ylabel('IU/A')
-yticks([0:3:15])
-axis([-1 10 0 18.75])
+yticks([0:3:18])
+axis([0 10 0 18])
 xticks([0:2:10])
 
 grid('on')
 
-%matlab2tikz
+matlab2tikz
